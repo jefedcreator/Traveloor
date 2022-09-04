921a29b4b7cfa06f1d8a7e07c80ac54989b31377
KishoreVB70 <kishore07kzr@gmail.com>
2022-09-04 07:42:55 +0530

    made minor changes

--- contracts/Traveloor.sol
+++ contracts/Traveloor.sol
@@ -13,22 +13,25 @@ contract Traveloor is ERC1155{
     Counters.Counter private _hotelIdCounter;
     Counters.Counter private _premiumIdCounter;
 
+    uint maximumNumOfTickets;
+
     uint256 public constant FLIGHT = 0;
     uint256 public constant CRUISE = 1;
     uint256 public constant HOTEL = 2;
     uint256 public constant TRAIN = 3;
     uint256 public constant PREMIUM = 4;
 
-    constructor() ERC1155("ipfs://Qmf6c4FJWRULNTQ4dWZ2zDEqpB16DwFWQGcyynrAwM6tut") {
-        // _mint(msg.sender, GOLD, 10**18, "");
-        // _mint(msg.sender, SILVER, 10**27, "");
-        // _mint(msg.sender, THORS_HAMMER, 1, "");
-        // _mint(msg.sender, SWORD, 10**9, "");
-        // _mint(msg.sender, SHIELD, 10**9, "");
+    constructor(uint _maxTickets) ERC1155("ipfs://Qmf6c4FJWRULNTQ4dWZ2zDEqpB16DwFWQGcyynrAwM6tut") {
+        maximumNumOfTickets = _maxTickets;
     }
 
     modifier checkPrice() {
         require(msg.value == 0.1 ether, "insufficient ether");
+        _;
+    }
+
+    modifier ifExceeded(uint _value) {
+        require(_value <= maximumNumOfTickets, "nft volume exceeded");
         _;
     }
 
@@ -53,40 +56,35 @@ contract Traveloor is ERC1155{
         ); 
     }
 
-    function mintFlight() public checkPrice() payable{
-        require(_flightIdCounter.current() <= 5, "nft volume exceeded");
+    function mintFlight() public checkPrice() payable ifExceeded(_flightIdCounter.current()){
         uint tokenId = _flightIdCounter.current();
         _flightIdCounter.increment();
         _setURI(uri(FLIGHT,tokenId));
         _mint(msg.sender, FLIGHT, 1,"");
     }
 
-    function mintTrain() public checkPrice() payable{
-        require(_trainIdCounter.current() <=5, "nft volume exceeded");
+    function mintTrain() public checkPrice() payable ifExceeded(_trainIdCounter.current()){
         uint tokenId = _trainIdCounter.current();
         _trainIdCounter.increment();
         _setURI(uri(TRAIN,tokenId));
         _mint(msg.sender, TRAIN, 1,"");
     }
 
-    function mintCruise() public checkPrice() payable{
-        require(_cruiseIdCounter.current() <= 5, "nft volume exceeded");
+    function mintCruise() public checkPrice() payable ifExceeded(_cruiseIdCounter.current()){ 
         uint tokenId = _cruiseIdCounter.current();
         _cruiseIdCounter.increment();
         _setURI(uri(CRUISE,tokenId));
         _mint(msg.sender, CRUISE, 1,"");
     }
 
-    function mintHotel() public checkPrice() payable{
-        require(_hotelIdCounter.current() <= 5, "nft volume exceeded");
+    function mintHotel() public checkPrice() payable ifExceeded(_hotelIdCounter.current()){
         uint tokenId = _hotelIdCounter.current();
         _hotelIdCounter.increment();
         _setURI(uri(HOTEL,tokenId));
         _mint(msg.sender, HOTEL, 1,"");
     }
 
-    function mintPremium() public payable{
-        require(_premiumIdCounter.current() <= 5, "nft volume exceeded");
+    function mintPremium() public payable ifExceeded(_premiumIdCounter.current()){
         require(msg.value == 0.3 ether, "insufficient ether");
         require(checkPremium());
         uint tokenId = _premiumIdCounter.current();
@@ -94,4 +92,5 @@ contract Traveloor is ERC1155{
         _setURI(uri(PREMIUM,tokenId));
         _mint(msg.sender, PREMIUM, 1,"");
     }
+
 }
